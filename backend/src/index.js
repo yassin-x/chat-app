9776eb116b913203ae299c54688cd5a19f7bbd95
Yassin Ibrahim <119485168+yassin-x@users.noreply.github.com>
2024-12-07 01:52:20 +0200

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,13 +25,6 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  })
-}
 
 server.listen(port, () => {
   console.log(`server is running port ${port}`);
